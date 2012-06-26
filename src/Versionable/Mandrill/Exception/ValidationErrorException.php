b4106c8d95c9680cdde9b84e783364e340b423c5
Harry Walter <harry.walter@lqdinternet.com>
2012-06-26 23:52:49 +0100

    ValidationError exception

---
+++ src/Versionable/Mandrill/Exception/ValidationErrorException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Versionable\Mandrill\Exception;
+
+/**
+ * Description of ValidationException
+ *
+ * @author Harry Walter <harry.walter@lqdinternet.com>
+ */
+class ValidationErrorException extends \InvalidArgumentException
+{
+    public function __construct($message, $code)
+    {
+        list($message, $json) = explode(": ", $message);
+        
+        $errors = "";
+        
+        foreach (json_decode($json) as $field => $error) {
+            $errors .= sprintf("\t%s: %s\r\n", $field, $error);
+        }
+        
+        parent::__construct(sprintf("%s:\r\n%s", $message, $errors), $code);
+    }
+}
