89780326417f6950607c5542c3d4b91bf40f688b
Harry Walter <harry.walter@lqdinternet.com>
2012-06-29 11:14:13 +0100

    Destruct objects into arrays

--- src/Versionable/Mandrill/Manager/Manager.php
+++ src/Versionable/Mandrill/Manager/Manager.php
@@ -3,6 +3,9 @@
 namespace Versionable\Mandrill\Manager;
 
 use Versionable\Mandrill\Client;
+use Versionable\Mandrill\Util\Inflector;
+
+use Versionable\Common\Collection\Set;
 
 /**
  * Description of Manager
@@ -28,8 +31,50 @@ abstract class Manager
         $this->client = $client;
     }
     
-    public function send($action, array $payload = array())
+    protected function doSend($action, Set $payload = null)
     {
+        if (null === $payload) {
+            $payload = array();
+        } else {
+            $payload = $this->buildPayload($payload);
+        }
+        
         return json_decode($this->client->send(static::API_BASE.$action, $payload));
+    }
+    
+    protected function buildPayload(Set $set)
+    {
+        $payload = array();
+        
+        foreach ($set->getIterator() as $field => $data) {
+            if ($data instanceof Set) {
+                $data = $this->buidPayload($data);
+            } elseif (true === is_object($data)) {
+                $data = $this->convertObject($data);
+            }
+            
+            $payload[Inflector::underscore($field)] = $data;
+        }
+        
+        return $payload;
+    }
+    
+    protected function convertObject($object)
+    {
+        $reflClass = new \ReflectionClass(get_class($object));
+        
+        $data = array();
+        foreach ($reflClass->getProperties(\ReflectionProperty::IS_PROTECTED) as $property) {
+            $property->setAccessible(true);
+            
+            $value = $property->getValue($object);
+            
+            if ($value instanceof Set) {
+                $value = $this->buildPayload($value);
+            }
+            $data[Inflector::underscore($property->getName())] = $value;
+        }
+        
+        return $data;
     }
 }
